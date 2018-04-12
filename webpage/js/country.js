93adbcaf2dc3c52a587347df98f42da867830262
Maria Varley <mvarley@gmail.com>
2018-04-12 15:38:36 +0100

    changed to new country selector

--- webpage/js/country.js
+++ webpage/js/country.js
@@ -1,37 +1,38 @@
-function setCountry(code){
-        if(code || code==''){
-            var text = jQuery('a[cunt_code="'+code+'"]').html();
-            $(".dropdown dt a span").html(text);
-        }
-    }
-    $(document).ready(function() {
-        $(".dropdown img.flag").addClass("flagvisibility");
+AmCharts.makeChart( "mapdiv", {
+  /**
+   * this tells amCharts it's a map
+   */
+  "type": "map",
 
-        $(".dropdown dt a").click(function() {
-            $(".dropdown dd ul").toggle();
-        });
+  /**
+   * create data provider object
+   * map property is usually the same as the name of the map file.
+   * getAreasFromMap indicates that amMap should read all the areas available
+   * in the map data and treat them as they are included in your data provider.
+   * in case you don't set it to true, all the areas except listed in data
+   * provider will be treated as unlisted.
+   */
+  "dataProvider": {
+    "map": "worldLow",
+    "areas": [
+      { "id": "IE" },
+      { "id": "CO" },
+      { "id": "MX" }
+    ]
+  },
 
-        $(".dropdown dd ul li a").click(function() {
-            //console.log($(this).html())
-            var text = $(this).html();
-            $(".dropdown dt a span").html(text);
-            $(".dropdown dd ul").hide();
-            $("#result").html("Selected value is: " + getSelectedValue("country-select"));
-        });
+  /**
+   * create areas settings
+   * autoZoom set to true means that the map will zoom-in when clicked on the area
+   * selectedColor indicates color of the clicked area.
+   */
+  "areasSettings": {
+    "autoZoom": true,
+    "selectedColor": "#CC0000"
+  },
 
-        function getSelectedValue(id) {
-            //console.log(id,$("#" + id).find("dt a span.value").html())
-            return $("#" + id).find("dt a span.value").html();
-        }
-
-        $(document).bind('click', function(e) {
-            var $clicked = $(e.target);
-            if (! $clicked.parents().hasClass("dropdown"))
-                $(".dropdown dd ul").hide();
-        });
-
-
-        $("#flagSwitcher").click(function() {
-            $(".dropdown img.flag").toggleClass("flagvisibility");
-        });
-    });
+  /**
+   * let's say we want a small map to be displayed, so let's create it
+   */
+  "smallMap": {}
+} );
